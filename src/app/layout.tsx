b99fb5d598d1a220352d63df4bed9e3a16445a4b
Duong Bao <tonbao0803@gmail.com>
2024-09-29 15:25:47 +0700

    fix: Fix chatbot config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import {
   Roboto_Slab,
   Ubuntu,
 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import LoadingWrapper from "@/components/LoadingWrapper";
 import { NotificationProvider } from "@/components/Notification";
@@ -14,7 +15,6 @@ import { Providers } from "@/redux/provider";
 import { NextProgressBar } from "@/components";
 import ClientLayout from "./auth/layout";
 
-const inter = Inter({ subsets: ["latin"] });
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -42,9 +42,9 @@ export default function RootLayout({
           </NotificationProvider>
         </LoadingWrapper>
 
-        <script>
-          {`!function(s,u,b,i,z){var o,t,r,y;s[i]||(s._sbzaccid=z,s[i]=function(){s[i].q.push(arguments)},s[i].q=[],s[i]("setAccount",z),r=["widget.subiz.net","storage.googleapis"+(t=".com"),"app.sbz.workers.dev",i+"a"+(o=function(k,t){var n=t<=6?5:o(k,t-1)+o(k,t-3);return k!==t?n:n.toString(32)})(20,20)+t,i+"b"+o(30,30)+t,i+"c"+o(40,40)+t],(y=function(k){var t,n;s._subiz_init_2094850928430||r[k]&&(t=u.createElement(b),n=u.getElementsByTagName(b)[0],t.async=1,t.src="https://"+r[k]+"/sbz/app.js?accid="+z,n.parentNode.insertBefore(t,n),setTimeout(y,2e3,k+1))})(0))}(window,document,"script","subiz", "acscryxkbryqwcqqwrkv")`}
-        </script>
+        <Script id="subiz-script" strategy="lazyOnload">
+          {`(function(s,u,b,i,z){var o,t,r,y;s[i]||(s._sbzaccid=z,s[i]=function(){s[i].q.push(arguments)},s[i].q=[],s[i]("setAccount",z),r=["widget.subiz.net","storage.googleapis"+(t=".com"),"app.sbz.workers.dev",i+"a"+(o=function(k,t){var n=t<=6?5:o(k,t-1)+o(k,t-3);return k!==t?n:n.toString(32)})(20,20)+t,i+"b"+o(30,30)+t,i+"c"+o(40,40)+t],(y=function(k){var t,n;s._subiz_init_2094850928430||r[k]&&(t=u.createElement(b),n=u.getElementsByTagName(b)[0],t.async=1,t.src="https://"+r[k]+"/sbz/app.js?accid="+z,n.parentNode.insertBefore(t,n),setTimeout(y,2e3,k+1))})(0))})(window,document,"script","subiz", "acscryxkbryqwcqqwrkv");`}
+        </Script>
       </body>
     </html>
   );
