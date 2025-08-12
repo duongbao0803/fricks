b02334305e478efd83dd0523ae479660364e7c10
duongbao0803 <tonbao0803@gmail.com>
2025-08-12 17:32:04 +0700

    refactor. refactor sitemap

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,5 +45,7 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|notfound).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|notfound|.*\\.xml$|robots\\.txt$).*)",
+  ],
 };
